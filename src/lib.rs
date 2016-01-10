280b1f70c559a285ab655ba37e7c0164123f2ecd
Mike Bush <mpbush@gmail.com>
2016-01-10 14:36:08 +0100

    Add method to move node between 2 link lists

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use std::ptr;
-
+use std::mem;
 
 pub struct FixedSizePriorityBuffer<T> {
     capacity: usize,
@@ -40,6 +40,10 @@ impl<T> Rawlink<T> {
             Some(&mut *self.p)
         }
     }
+
+    fn is_none(&self) -> bool {
+        self.p.is_null()
+    }
 }
 
 impl<T> Node<T> {
@@ -72,11 +76,77 @@ fn link_no_prev<T>(mut next: Box<Node<T>>) -> Link<T> {
     Some(next)
 }
 
+
+fn insert_slice<T>(range_start: &mut Link<T>, range_end: &mut Rawlink<Node<T>>,
+                   target_start: &mut Link<T>, target_end: &mut Rawlink<Node<T>>) {
+    if range_start.is_none() {
+        panic!("range_start must not be none");
+    }
+    if range_end.is_none() {
+        panic!("range_end must not be none");
+    }
+    
+
+    unsafe {
+        // dereference the end node first as the pointer will be changed when rotating the backwards links
+        let before_end_node: *mut Node<T> = 
+            range_end.resolve_mut().expect("source list to not be emptyi: end is none");
+        // rotate backawrds links first as we have captured the pointer to the end node
+        {
+            let start_node: &mut Box<Node<T>> = &mut 
+                range_start.as_mut().expect("source list to not be empty: start is none");
+            let mut temp: Rawlink<Node<T>> = mem::uninitialized();
+            ptr::copy_nonoverlapping(&start_node.prev, &mut temp, 1);
+            ptr::copy_nonoverlapping(&*target_end, &mut start_node.prev, 1);
+            ptr::copy_nonoverlapping(&*range_end, target_end, 1);
+            ptr::copy_nonoverlapping(&temp, range_end, 1);
+
+            mem::forget(temp);
+        }
+
+        // rotate forwards links
+        {
+            let mut temp: Link<T> = mem::uninitialized();
+            ptr::copy_nonoverlapping(&*range_start, &mut temp, 1);
+            ptr::copy_nonoverlapping(&(*before_end_node).next, range_start, 1); 
+            ptr::copy_nonoverlapping(&*target_start, &mut (*before_end_node).next, 1);
+            ptr::copy_nonoverlapping(&temp, target_start, 1); 
+
+            mem::forget(temp);
+        }
+    }
+}
+
+#[cfg(test)]
+mod swap_tests {
+    use super::Node;
+    use super::link_no_prev;
+    use super::Rawlink;
+    use super::insert_slice;
+
+    #[test]
+    fn swaps_single_item_list_with_empty() {
+        let mut single_node = Box::new(Node::new("Single node"));
+        let mut single_end = Rawlink::some(single_node.as_mut());
+        let mut single_start = link_no_prev(single_node);
+        let mut empty_start = None;
+        let mut empty_end = Rawlink::none();
+
+        insert_slice(&mut single_start, &mut single_end, &mut empty_start, &mut empty_end);
+
+        assert!(single_start.is_none());
+        assert!(single_end.is_none());
+        let empty_start = empty_start.expect("the node should have been moved");
+        assert_eq!(empty_start.value.expect("some value on node"), "Single node");
+    }
+}
+
+
 impl<T> FixedSizePriorityBuffer<T> {
     pub fn new(capacity: usize) -> FixedSizePriorityBuffer<T> {
         let mut first_empty = Box::new(Node::<T>::empty());
         let last_empty = Rawlink::some(first_empty.as_mut());
-        for i in 1..capacity {
+        for _ in 1..capacity {
             let mut next_empty = Box::new(Node::<T>::empty());
             next_empty.set_next(first_empty);
             first_empty = next_empty;
@@ -102,6 +172,9 @@ impl<T> FixedSizePriorityBuffer<T> {
     }
 
     pub fn enqueue(&mut self, element: T) {
+        if self.size >= self.capacity {
+            panic!("No remaining capacity to enqueue");
+        }
         let new_node = Box::new(Node::new(element));
         match unsafe { self.last.resolve_mut() } {
             None => {
